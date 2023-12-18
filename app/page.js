41ffd3f000b3945d1ec64d17412d79f773061aa7
coffeemug.tester <daniel.alfaro@adevinta.com>
2023-12-19 00:33:42 +0100

    add tailwind

--- app/page.js
+++ app/page.js
@@ -17,93 +17,48 @@ export default async function Home() {
   }
 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className="container mx-auto p-4">
+        <div className="flex flex-col gap-4">
+          {/* Text Block */}
+          <div className="p-4 border rounded shadow-sm">
+            <p className="text-gray-700">Some text content...</p>
+            <p className="text-gray-700">Rating: {dataObject.result.rating}</p>
+          </div>
+
+          {/* Ratings Block */}
+          <div className="p-4 border rounded shadow-sm">
+            <p className="text-gray-700">Text (ratings)</p>
+          </div>
+
+          {/* Photos Block */}
+          <div className="p-4 border rounded shadow-sm">
+            <div className="w-full h-64 bg-gray-200 flex justify-center items-center">
+              <span>Picture (Photos)</span>
+            </div>
+          </div>
+
+          {/* Working Hours Comparison Block */}
+          <div className="p-4 border rounded shadow-sm">
+            <p className="text-gray-700">Working hours: comparison</p>
+          </div>
+
+          {/* Google Maps Embed Block */}
+          <div className="p-4 border rounded shadow-sm">
+            {/* Replace `src` with the actual Google Maps embed link */}
+            <iframe
+                title="Google Map"
+                className="w-full h-64"
+                frameBorder="0"
+                src="https://www.google.com/maps/embed?pb=!1m18..."
+                allowFullScreen
+            ></iframe>
+          </div>
+
+          {/* Metrics Block */}
+          <div className="p-4 border rounded shadow-sm">
+            <p className="text-gray-700">Metrics</p>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   )
 }
