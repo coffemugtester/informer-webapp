f8cbfd82e1d89748a49b7003ddd592d74169edb5
coffeemug.tester <daniel.alfaro@adevinta.com>
2023-12-26 12:59:09 +0100

    add grid layout divs

--- app/page.js
+++ app/page.js
@@ -17,48 +17,13 @@ export default async function Home() {
   }
 
   return (
-      <div className="container mx-auto p-4">
-        <div className="flex flex-col gap-4">
-          {/* Text Block */}
-          <div className="p-4 border rounded shadow-sm">
-            <p className="text-gray-700">Some text content...</p>
-            <p className="text-gray-700">Rating: {dataObject.result.rating}</p>
-          </div>
-
-          {/* Ratings Block */}
-          <div className="p-4 border rounded shadow-sm">
-            <p className="text-gray-700">Text (ratings)</p>
-          </div>
-
-          {/* Photos Block */}
-          <div className="p-4 border rounded shadow-sm">
-            <div className="w-full h-64 bg-gray-200 flex justify-center items-center">
-              <span>Picture (Photos)</span>
-            </div>
-          </div>
-
-          {/* Working Hours Comparison Block */}
-          <div className="p-4 border rounded shadow-sm">
-            <p className="text-gray-700">Working hours: comparison</p>
-          </div>
-
-          {/* Google Maps Embed Block */}
-          <div className="p-4 border rounded shadow-sm">
-            {/* Replace `src` with the actual Google Maps embed link */}
-            <iframe
-                title="Google Map"
-                className="w-full h-64"
-                frameBorder="0"
-                src="https://www.google.com/maps/embed?pb=!1m18..."
-                allowFullScreen
-            ></iframe>
-          </div>
-
-          {/* Metrics Block */}
-          <div className="p-4 border rounded shadow-sm">
-            <p className="text-gray-700">Metrics</p>
-          </div>
-        </div>
+      <div className="grid-container">
+        <div className="container-a"></div>
+        <div className="container-b"></div>
+        <div className="container-c"></div>
+        <div className="container-d"></div>
+        <div className="container-e"></div>
+        <div className="container-f"></div>
       </div>
   )
 }
