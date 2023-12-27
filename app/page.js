39f9bcf0775aa39f6d303ae92e26f3d9442f1d72
coffeemug.tester <daniel.alfaro@adevinta.com>
2023-12-27 18:15:14 +0100

    move grid layout into layout.js

--- app/page.js
+++ app/page.js
@@ -8,9 +8,10 @@ import {MainCard} from "../components/MainCard";
 import {WorkingHoursCard} from "../components/WorkingHoursCard";
 
 export default async function Home() {
-
+  //TODO: fix variables' names
   let dataObject;
 
+  //TODO: wrap this conditionals in functions in a utils.js
   if (process.env.IS_MOCK_DATA) {
     console.log(`isMockData: ${process.env.IS_MOCK_DATA}`)
     dataObject = mockData
@@ -22,13 +23,13 @@ export default async function Home() {
   }
 
   return (
-      <div className="grid-container">
+      <>
         <PageName/>
         <RatingAndReviewsCount dataObject={dataObject}/>
         <WorkingHoursCard dataObject={dataObject}/>
         <MainCard dataObject={dataObject}/>
-        <MapCard coordinates={dataObject.result.geometry.location}/>
+        {/*<MapCard coordinates={dataObject.result.geometry.location}/>*/}
         <Rankings/>
-      </div>
+      </>
   )
 }
