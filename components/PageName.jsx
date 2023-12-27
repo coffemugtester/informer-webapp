c024e0ede2a26de571e8a605514be67e314a08cc
coffeemug.tester <daniel.alfaro@adevinta.com>
2023-12-27 11:07:41 +0100

    finish main card mock up

--- components/PageName.jsx
+++ components/PageName.jsx
@@ -7,7 +7,7 @@ export const PageName = () => {
         placeItems: 'center',
         height: '100%'  // Or any specific height as needed
       }}>
-        <h1 style={{ color: 'white', fontWeight: 'bold', fontFamily: 'Georgia, serif', fontSize: '2rem' }}>Informer</h1>
+        <h1 style={{ color: 'white', fontWeight: '500', fontFamily: '"Mona Sans", "Helvetica Neue", Helvetica, Arial, sans-serif', fontSize: '2rem' }}>Resumen</h1>
       </div>
   )
 }
--- components/RatingAndReviewsCount.jsx
+++ components/RatingAndReviewsCount.jsx
@@ -2,12 +2,27 @@ import React from "react";
 
 export const RatingAndReviewsCount = ({dataObject}) => {
   return (
-      <div className="container-b" style={{
-        display: 'grid',
-        placeItems: 'center',
-        height: '100%'  // Or any specific height as needed
-      }}>
-        <h1 style={{ color: 'black', fontWeight: 'bold', fontFamily: 'Georgia, serif', fontSize: '2rem' }}>Rating: {dataObject.result.rating} - N°Reviews: {dataObject.result.user_ratings_total}</h1>
+      <div className="container-b" style={{placeItems: 'center'}} >
+        <div style={{width: '100%', gridColumnStart: 1, gridColumnEnd: 2, gridRowStart: 2, gridRowEnd: 3, borderRight: '1px solid silver', textAlign: 'center', fontSize: '1.2rem'}}>
+          <h2>
+            <span className="material-symbols-outlined" style={{verticalAlign: 'middle', fontSize: '1.5rem', paddingBottom: '0.3rem'}}>sentiment_neutral</span>&nbsp;
+            {dataObject.result.rating}/5
+            <span className="material-symbols-outlined" style={{fontSize: '1.2rem'}}>grade</span>
+            <span className="material-symbols-outlined" style={{fontSize: '1.2rem'}}>grade</span>
+            <span className="material-symbols-outlined" style={{fontSize: '1.2rem'}}>grade</span>
+          </h2>
+        </div>
+        <div style={{width: '100%', gridColumnStart: 2, gridColumnEnd: 3, gridRowStart: 2, gridRowEnd: 3, textAlign: 'center', fontSize: '1.2rem'}}>
+          <h2>
+            <span className="material-symbols-outlined" style={{verticalAlign: 'middle', fontSize: '1.5rem'}}>rate_review</span>&nbsp;
+            {dataObject.result.user_ratings_total}
+            <span style={{fontSize: '1rem'}}> (opiniones)</span>
+          </h2>
+        </div>
+        {/*
+        <h1 style={{ color: 'black', fontWeight: 'bold', fontFamily: '"Mona Sans", "Helvetica Neue", Helvetica, Arial, sans-serif', fontSize: '2rem' }}>Rating: {dataObject.result.rating} - N°Reviews: {dataObject.result.user_ratings_total}</h1>
+
+        */}
       </div>
   )
 }
