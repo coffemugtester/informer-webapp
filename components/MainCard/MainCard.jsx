00ac859ba66053d1a3f510ed484cfe6cdf445bc2
coffeemug.tester <daniel.alfaro@adevinta.com>
2023-12-28 19:31:25 +0100

    fix p styling error

--- components/MainCard/MainCard.jsx
+++ components/MainCard/MainCard.jsx
@@ -16,7 +16,7 @@ const MainCard = ({formattedAddress, rating}) => {
             </div>
           </div>
           <div className='secondSnippet'>
-            <p>{rating}/5 <span style={{fontSize: '0.75rem'}}>(valoración promedia)</span></p>
+            <p style={{color: "white", gridColumnStart: 1, gridRowStart: 2, gridRowEnd: 3}}>{rating}/5 <span style={{fontSize: '0.75rem'}}>(valoración promedia)</span></p>
             <div className='comparison'>
               <p style={{color: 'white'}}> - hrs. - Otras misiones</p>
               <p style={{color: 'white'}}> - hrs. - Otros países</p>
